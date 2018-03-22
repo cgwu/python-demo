2a1c15f42e6a17619c84d29a48176f92c8b585cd
dannis wu <superwcg@gmail.com>
2018-03-22 15:31:21 +0800

    log test ok. P217

--- django-demos/COFFEE_HOUSE/coffeehouse/jj2app/views.py
+++ django-demos/COFFEE_HOUSE/coffeehouse/jj2app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+# Python logging package
+import logging
+# Standard instance of a logger with __name__
+stdlogger = logging.getLogger(__name__)
 
 # Create your views here.
 def foo(request):
+    stdlogger.warning('您访问了jj2app/foo!')
     return render(request, 'jj2app/foo.html')
 
--- django-demos/COFFEE_HOUSE/coffeehouse/settings.py
+++ django-demos/COFFEE_HOUSE/coffeehouse/settings.py
@@ -178,3 +178,78 @@ DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '//apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js',
 }
 
+# 日志设置
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '[%(asctime)s] %(levelname)s %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'verbose': {
+            'format': '[%(asctime)s.%(msecs)d] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'development_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/django_dev.log',
+            'formatter': 'verbose'
+        },
+        'production_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/django/django_production.log',
+            'maxBytes' : 1024*1024*100, # 100MB
+            'backupCount' : 5,
+            'formatter': 'simple'
+        },
+        'dba_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_false','require_debug_true'],
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/django/dba.log',
+            'formatter': 'simple'
+        },
+    },
+    'root': {
+        #'level': 'DEBUG',
+        'level': 'WARNING',
+        'handlers': ['console'],
+    },
+    'loggers': {
+        'coffeehouse': {
+            'handlers': ['development_logfile','production_logfile'],
+         },
+        'dba': {
+            'handlers': ['dba_logfile'],
+        },
+        'django': {
+            'handlers': ['development_logfile','production_logfile'],
+        },
+        'py.warnings': {
+            'handlers': ['development_logfile'],
+        },
+    }
+}
+
+

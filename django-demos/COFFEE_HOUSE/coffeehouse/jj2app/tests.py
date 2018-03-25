f03285af9bf92caa53103c37d39356b9cf2477a4
dannis wu <superwcg@gmail.com>
2018-03-25 11:26:29 +0800

    testcase save data NOT OK

--- django-demos/COFFEE_HOUSE/coffeehouse/jj2app/tests.py
+++ django-demos/COFFEE_HOUSE/coffeehouse/jj2app/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase,TransactionTestCase
-from django.db import transaction
+#from django.test.utils import override_settings
+#from django.db import transaction
+import time
 
 #from coffeehouse.jj2app.models import Store
 from .models import Store
@@ -12,20 +14,23 @@ from .models import Store
 #import logging
 #LOG = logging.getLogger(__name__)
 
-#class StoreTestCase(TestCase):
-class StoreTestCase(TransactionTestCase):
+class StoreTestCase(TestCase):
+#class StoreTestCase(TransactionTestCase):
     def setUp(self):
 #        LOG.debug('=== setup ===')
         print('启动设置setup ...')
+        Store.objects.create(name='大郎烧饼8', address='沉香street#8') # Create
 
     #@transaction.commit_on_success
+    #@override_settings(DEBUG=False)
     def test_save(self):
 #        LOG.debug('test save store')
         print('test save store')
         store1 = Store(name='大郎烧饼', address='沉香street#1')
         #with transaction.commit_on_success():
-        store1.save()
+        store1.save()  # create or update
         print('#id', store1.id)
+        #time.sleep(60)
 
     def tearDown(self):
 #        LOG.debug('=== tear down ===')
